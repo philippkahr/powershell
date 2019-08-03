dcf1f798531e496ffcff66c285b82880df57aae3
Philipp Kahr <philipp.kahr@icloud.com>
2019-08-03 13:57:15 +0200

    Initial commit

---
+++ ActiveDirectory/Resolve-UnknownNSEntries.ps1
@@ -0,0 +1,56 @@
+﻿<#
+.SYNOPSIS
+  This scripts searches for entries that are from the "NS" type. This is needed to verify that the domain soa record
+  does not contain any unknown entries.
+
+.DESCRIPTION
+  1. This script asks for an input e.g. "domain.local"
+  2. Then it searches for any "NS" type record
+  3. Foreach found NS record it verifies if the IPAddress contains an "unkown" entry.
+  4. Output the 
+
+.PARAMETER domainName
+  Required as it searches for the domain SOA record.
+
+.NOTES
+  Version:        1.0
+  Author:         Philipp Kahr
+  Creation Date:  03.08.2019
+  Purpose/Change: Search for unknown NS entries
+  
+.EXAMPLE
+  Resolve-UnknownNSEntries.ps1
+#>
+
+$sScriptVersion = "1.0"
+
+$domainName = Read-Host "Insert the domain name"
+
+function Report-Errors(){
+    Param(
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true)]
+        $domainName
+    )
+    try{ 
+        [int] $errorsFound=0
+        $dnsRecord = Get-DnsServerResourceRecord -RRType NS -ZoneName $domainName 
+        foreach($dnsEntry in $dnsRecord.RecordData){
+            $temp = Resolve-DnsName -Name $dnsEntry.NameServer -Server 127.0.0.1 -ErrorAction SilentlyContinue -ErrorVariable domainError
+            if($domainError){
+                $outputString = "Error found with: " + $dnsEntry.NameServer
+                echo $outputString
+                $errorsFound = 1
+            }
+        }
+        if($errorsFound -eq 0){
+          echo "Could not find any unknown entries"
+        }
+    }catch{
+      $ErrorMessage = $_.Exception.Message
+      $FailedItem = $_.Exception.ItemName
+      Write-Output $ErrorMessage
+      Write-Output $FailedItem
+    }
+  }
+Report-Errors -domainName $domainName
---
+++ DHCPServer/Manage-DHCPServer.ps1
@@ -0,0 +1,51 @@
+﻿<#
+.SYNOPSIS
+  This script takes the inserted hostname and searches for the dhcp config. It can import and export that config.
+
+.DESCRIPTION
+  Export
+  1. This script asks for a hostname
+  2. It will create a folder structure like "C:/temp/dhcp/hostname/"
+  3. It will put the export as an xml inside that folder
+  Import
+  1. This script asks for a hostname
+  2. It will automatically search for a config file in the same directory as the export
+  3. It will create a popup asking for confirmation.
+
+.PARAMETER hostname
+  Required as it connects to the DHCP server.
+
+.NOTES
+  Version:        1.0
+  Author:         Philipp Kahr
+  Creation Date:  03.08.2019
+  Purpose/Change: Search for unknown NS entries
+  
+.EXAMPLE
+  Manage-DHCPServer.ps1
+#>
+
+$sScriptVersion = "1.0"
+
+Import-module dhcpserver
+
+$hostname = Read-Host "Enter the host"
+$importOrExport = Read-Host "Do you want to import or export, answer with 'import' or 'export'"
+$pathForConfigFolder = "C:\temp\dhcp\" + $hostname
+$pathForConfigFile = $pathForConfigFolder + "\" + $hostname + "_config.xml"
+$pathForBackupFile = $pathForConfigFolder + "\" + $hostname + "_backup.xml"
+
+
+if($importOrExport.Equals("export")){
+    echo "Going to export the configuration from $hostname"
+
+    New-Item -ItemType directory -Path $pathForConfigFolder -Force
+
+    Export-DhcpServer -ComputerName $hostname -Leases -File $pathForConfigFile -Force
+}
+if($importOrExport.Equals("import")){
+
+    echo "Going to IMPORT the configuration from $hostname"
+
+    Import-DhcpServer -ComputerName $hostname -Leases -File $pathForConfigFile -BackupPath $pathForBackupFile
+}
